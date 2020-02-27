73b4cc4032f9c160ebc623ce49b2b258e75b6c6a
Jacob Paine <painejacob@gmail.com>
2020-02-26 18:09:00 -0600

    Changes port to 5000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var io = require('socket.io').listen(server);
 var users = [];
 var connections = [];
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 5000);
 console.log('server runnning...');
 
 app.get('/', function (req, res) {
