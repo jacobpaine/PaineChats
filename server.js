c7caeb76849aa71970fbd9115c976329307072b7
Jacob Paine <painejacob@gmail.com>
2020-02-26 18:52:05 -0600

    Log in server

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.sockets.on('connection', function (socket) {
     // Disconnect
     socket.on('disconnect', function (data) {
         if (!socket.username) return;
+        console.log('data: ', data);
         users.splice(users.indexOf(socket.username), 1);
         updateUsernames()
         connections.splice(connections.indexOf(socket), 1);
@@ -27,7 +28,6 @@ io.sockets.on('connection', function (socket) {
     })
 
     socket.on('send message', function (data) {
-        // console.log(data)
         io.sockets.emit('new message', { msg: data, user: socket.username });
     });
 
